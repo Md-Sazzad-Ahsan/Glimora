3c5a7eeac7be78f57874393e85408dd8a1d2d291
Md-Sazzad-Ahsan <himu200037@diit.edu.bd>
2025-05-22 01:44:29 +0600

    Remove Border from sidebar

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -41,13 +41,13 @@ const Sidebar = ({ isOpen, toggleSidebar, chats, onSelectChat, onNewChat, active
       
       {/* Sidebar */}
       <div 
-        className={`fixed top-0 left-0 bottom-0 w-64 bg-gray-50 border-r border-gray-200 dark:bg-gray-900 dark:border-gray-700 transform transition-transform duration-300 ease-in-out z-50 lg:z-30 ${
+        className={`fixed top-0 left-0 bottom-0 w-64 bg-gray-50 dark:bg-gray-900 shadow-lg transform transition-transform duration-300 ease-in-out z-50 lg:z-30 ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
         <div className="flex flex-col h-full">
           {/* Sidebar Header with Close Button */}
-          <div className="h-16 flex items-center px-4 border-b border-gray-200 dark:border-gray-700">
+          <div className="h-16 flex items-center px-4">
             <button
               onClick={toggleSidebar}
               className="text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
